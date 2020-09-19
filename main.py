a48eb1f9b8dd5696c6ef3e721debe2b2beacbff2
nebula1989 <benwalterscoding@gmail.com>
2020-09-18 20:40:35 -0400

    label that displays slider value, asthetic fixes

--- main.py
+++ main.py
@@ -13,25 +13,25 @@ window.resizable(False, False)
 #  photo stuff
 fortnite_img_path = "resources/fortnite.jpg"
 img = ImageTk.PhotoImage(Image.open(fortnite_img_path))
-Label(window, image=img, bg="black").pack()
+Label(window, image=img).pack()
 
 
 # slider knob
-value = IntVar()
 
+slider_value = IntVar()
 slider = Scale(
     window,
-    from_=100, to=10000,
-    resolution=100,
+    from_=1000, to=100000,
+    resolution=1000,
     orient=HORIZONTAL,
-    variable=value,
+    variable=slider_value,
     bg="royal blue",
-    width=75,
+    width=50,
     length=350,
     font=("Arial Black", 14)
 )
 
-slider.place(x=100, y=375)
+slider.place(x=75, y=410)
 
 
 def display_key():
@@ -39,20 +39,39 @@ def display_key():
     on click, display the key
     """
 
+    # generate the codes
     first4 = secrets.token_hex(2)
     second4 = secrets.token_hex(2)
     third4 = secrets.token_hex(2)
     fourth4 = secrets.token_hex(2)
 
-    key_text = Text(window, height=1, width=21, borderwidth=2, font=("Helvetica", 14))
+    # display the and label and generated key text
+    key_label = Label(
+        text=("The Code for " + str(slider_value.get()) + " V-Coins is"),
+        font=("Arial Black", 12),
+        bg="grey79",
+
+
+        )
+    key_label.place(x=90, y=330)
+
+    # key text
+    key_text = Text(
+        window,
+        height=1,
+        width=21,
+        borderwidth=2,
+        font=("Impact", 22),
+        bg="grey79",
+
+    )
     key_text.insert(1.0, (" " + first4 + " - " + second4 + " - " + third4 + " - " + fourth4))
-    key_text.place(x=125, y=275)
-    key_text.configure(state="disabled")
+    key_text.place(x=85, y=360)
 
 
 # button
 genkey_btn = Button(window, text="Generate a Key", command=display_key, bg="PaleGreen2", font=("Impact", 14))
-genkey_btn.place(x=200, y=500)
+genkey_btn.place(x=175, y=500)
 
 
 window.mainloop()
